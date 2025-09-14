06158cdbbea13a37cae52be25d22bd4e58f41e47
Rohit Mukkala <mukkalarohit@gmail.com>
2025-09-14 11:41:37 +0530

    cleaned

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,90 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Fueling ideas with innovation, I craft intelligent systems and
+            seamless experiences, blending logic with creativity to solve
+            real-world challenges and inspire progress.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2026 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            Pursuing my B.Tech in Computer Science Engineering (CSE), where I am
+            expanding my expertise in software development, algorithms, data
+            structures, and emerging technologies, preparing to contribute
+            meaningfully to the tech industry.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2022 Higher Secondary Education",
+      content: (
+        <>
+          <p>
+            Completed my Higher Secondary Education with a focus on Science,
+            excelling in subjects like Physics, Mathematics, and Computer
+            Science, which laid a strong foundation for my journey into
+            technology.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2024 Business Development Intern",
+      content: (
+        <>
+          <p>
+            Developed business strategies, streamlined communication processes,
+            and built client relationships to improve business outcomes during
+            my internship at Younity.in.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2024 AI/ML Intern",
+      content: (
+        <>
+          <p>
+            During my internship at YBI Foundation, I enhanced my web
+            development skills by integrating machine learning models into web
+            applications, improving my backend and API development expertise.
+            This experience helped me create data-driven, efficient web
+            solutions, strengthening my ability to build dynamic and responsive
+            applications.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2023 Web Developer & Game Builder (Practice Projects)",
+      content: (
+        <>
+          <p>
+            Developed several practice web applications, including a To-Do List
+            App, Calculator, and a game platform with PIG Game, Connect 4, and
+            Guess My Number. Additionally, I built a responsive Ecommerce
+            Website front-end, refining my skills in React.js, Node.js, and
+            MongoDB, and strengthening my understanding of full-stack
+            development.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
